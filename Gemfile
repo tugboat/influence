4bbd0ba5397a22740964bd8f169f323dc2cce2be
Wayne Schroer <wayneschroer@gmail.com>
2012-11-27 22:50:55 -0600

    Added some calls to the api

--- Gemfile
+++ Gemfile
@@ -1,2 +1,7 @@
 source :rubygems
+
+gem 'rest-client'
+gem 'gli'
+gem 'terminal-table'
+
 gemspec
--- lib/influence.rb
+++ lib/influence.rb
@@ -1,4 +1,5 @@
 require 'influence/version.rb'
+require 'influence/app.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
